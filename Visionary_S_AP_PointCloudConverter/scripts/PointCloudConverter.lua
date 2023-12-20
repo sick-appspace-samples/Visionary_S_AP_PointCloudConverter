44f5d765cb326660f278e7cef5465a2e0aeaf5ea
SICKAppSpace-dev <>
2023-12-20 23:59:24 +0000

    Update

--- Visionary_S_AP_PointCloudConverter/scripts/PointCloudConverter.lua
+++ Visionary_S_AP_PointCloudConverter/scripts/PointCloudConverter.lua
@@ -1,28 +1,28 @@
 --[[----------------------------------------------------------------------------
 
   Application Name: Visionary_S_AP_PointCloudConverter
-  
+
   Summary:
   Show how to calculate a full pointcloud and only a subpart of the image as pointloud in Lua
-  
+
   Description:
   Set up the camera to take live images continuously and automatically calculate
   pointclouds out of it. First the full Z image is converted to a pointcloud
   and shown via the left Viewer. As second sample only a subset of the image
   in form of a pixel region is converted to a pointcloud to show how to save
   performance and convert only what you need to a pointcloud.
-  
+
   How to run:
   Start by running the app (F5) or debugging (F7+F10).
   Set a breakpoint on the first row inside the main function to debug step-by-step.
   See the results in the different 3D viewer on the DevicePage.
-  
+
   More Information:
   If you want to run this app on an emulator some changes are needed to get images.
-    
+
 ------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
-  
+
   --uncomment Log.setLevel if you want to see the number of points in Log.info in the handleOnNewImage function
   --Log.setLevel("INFO")
 
@@ -59,7 +59,8 @@ Script.register("Engine.OnStarted", main)
 
 --------------------------------------------------------------------------------
 
---@handleOnNewImage(image:Image,sensordata:SensorData)
+---@param image Image
+---@param sensordata SensorData
 local function handleOnNewImage(image)
   -- calculate the full point cloud and color it with the distance values
   local pointCloud = pc_converter:toPointCloud(image[1], image[1])
